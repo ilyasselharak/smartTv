b3b18d4f591ed9cfbe3d0456a1f77f540cf23bca
ilyass el harak <ilyasselharak@gmail.com>
2023-05-15 17:17:11 +0300

    image hight

--- pages/index.js
+++ pages/index.js
@@ -66,10 +66,10 @@ export default function Home({ packages }) {
         />
       </Head>
       <Header />
-      <div className="top-20 sm:w-[730px] xl:w-full md:w-full 2xl:w-ful lg:w-full w-[900px] xl:top- right-0 relative">
+      <div className="top-20 sm:w-[730px] xl:w-full md:w-full 2xl:w-ful lg:w-full w-[750px] xl:top- right-0 relative">
         <Swiper
           style={{ "zIndex": "0", }}
-          className="h-[675px]"
+          className="h-[469px] xl:h-[600px]"
           autoplay={{
             delay: 6000,
             disableOnInteraction: false,
