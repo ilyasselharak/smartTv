c824cd2cae39cf4c3cb7ed639eff74a721144e39
ilyass el harak <ilyasselharak@gmail.com>
2023-05-19 17:25:23 +0300

    config script

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { Helmet } from 'react-helmet'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { Helmet } from "react-helmet";
 
 export default function Document() {
   return (
-    <Html lang="en" style={{scrollBehavior: 'smooth'}}>
-      <Head >
-      <Helmet>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11110494127"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'AW-11110494127'); </script>
-      </Helmet>
+    <Html lang="en" style={{ scrollBehavior: "smooth" }}>
+      <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-11110494127"
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-11110494127');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
