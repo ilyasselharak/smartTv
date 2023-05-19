c058039fb75a2fe633e1f26186dbae345cbf5dd0
ilyass el harak <ilyasselharak@gmail.com>
2023-05-19 17:17:10 +0300

    added script of google

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
     <Html lang="en" style={{scrollBehavior: 'smooth'}}>
       <Head >
       <Helmet>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11110494127"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11110494127'); </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11110494127"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'AW-11110494127'); </script>
       </Helmet>
       </Head>
       <body>
