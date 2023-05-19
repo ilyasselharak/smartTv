10e8eeb890f42de1d9c70955b166e65e3b0e4cbd
ilyass el harak <ilyasselharak@gmail.com>
2023-05-19 17:16:02 +0300

    added script of google

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Helmet } from 'react-helmet'
 
 export default function Document() {
   return (
     <Html lang="en" style={{scrollBehavior: 'smooth'}}>
-      <Head />
+      <Head >
+      <Helmet>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11110494127"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11110494127'); </script>
+      </Helmet>
+      </Head>
       <body>
         <Main />
         <NextScript />
